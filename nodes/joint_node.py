27f28008c6a2ecd82c80598bc982dfb3b8080331
Katelyn <katelynslee@gmail.com>
2023-12-13 17:25:23 -0500

    fixed joint script

--- nodes/joint_node.py
+++ nodes/joint_node.py
@@ -7,50 +7,52 @@ import geometry_msgs.msg
 import std_msgs.msg
 import transforms3d as td
 import numpy as np
-#roslib.load_manifest('trakstar')
 
+class JointAngle(object):
+    def __init__(self):
+        # Subscribing to TF
+        self.tfBuffer = tf2_ros.Buffer()
+        listener = tf2_ros.TransformListener(self.tfBuffer)
 
-def to_affine(t):
-    T = [t.transform.translation.x, t.transform.translation.y, t.transform.translation.z]
-    rotation = [t.transform.rotation.w, t.transform.rotation.x, t.transform.rotation.y, t.transform.rotation.z]
-    R = td.quaternions.quat2mat(rotation)
-    Z = np.ones(3)
+        # Publishing joint angle
+        self.joint_angle = rospy.Publisher('joint_angle', std_msgs.msg.Float32, queue_size = 1)
 
-    return td.affines.compose(T, R, Z)
+        b_sensor0_i = self.to_affine(self.tfBuffer.lookup_transform('trakstar_base', 'trakstar0', rospy.Time(0), rospy.Duration(3.0)))
+        b_sensor1_i = self.to_affine(self.tfBuffer.lookup_transform('trakstar_base', 'trakstar1', rospy.Time(0), rospy.Duration(3.0)))
 
+        self.s0_to_s1_i = np.dot(np.linalg.inv(b_sensor0_i), b_sensor1_i)
 
-def jnode():
-    rospy.init_node('joint_node')
-    tfBuffer = tf2_ros.Buffer()
-    listener = tf2_ros.TransformListener(tfBuffer)
+    def to_affine(self, t):
+        T = [t.transform.translation.x, t.transform.translation.y, t.transform.translation.z]
+        rotation = [t.transform.rotation.w, t.transform.rotation.x, t.transform.rotation.y, t.transform.rotation.z]
+        R = td.quaternions.quat2mat(rotation)
+        Z = np.ones(3)
 
-    joint_angle = rospy.Publisher('joint_angle', std_msgs.msg.Float32, queue_size = 1)
+        return td.affines.compose(T, R, Z)
 
-    rate = rospy.Rate(10.0)
-    b_sensor0_i = to_affine(tfBuffer.lookup_transform('trakstar_base', 'trakstar0', rospy.Time(0), rospy.Duration(3.0)))
-    b_sensor1_i = to_affine(tfBuffer.lookup_transform('trakstar_base', 'trakstar1', rospy.Time(0), rospy.Duration(3.0)))
-    
-    s0_to_s1_i = np.dot(np.linalg.inv(b_sensor0_i), b_sensor1_i)
-    
-    while not rospy.is_shutdown():
-        try:
-            b_sensor0 = to_affine(tfBuffer.lookup_transform('trakstar_base', 'trakstar0', rospy.Time(0)))
-            b_sensor1 = to_affine(tfBuffer.lookup_transform('trakstar_base', 'trakstar1', rospy.Time(0)))
-        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
-            continue
+    def angle_calc(self):
 
-        s0_to_s1 = np.dot(np.linalg.inv(b_sensor0), b_sensor1)
-        angle_tf = np.dot(np.linalg.inv(s0_to_s1_i), s0_to_s1)
-        
-        ax, angle, pt = td.axangles.aff2axangle(angle_tf)
+        while not rospy.is_shutdown():
+            try:
+                b_sensor0 = self.to_affine(self.tfBuffer.lookup_transform('trakstar_base', 'trakstar0', rospy.Time(0)))
+                b_sensor1 = self.to_affine(self.tfBuffer.lookup_transform('trakstar_base', 'trakstar1', rospy.Time(0)))
 
-        msg = std_msgs.msg.Float32()
-        msg.data = angle * (180 / np.pi)
+            except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+                continue
 
-        joint_angle.publish(msg)
+            s0_to_s1 = np.dot(np.linalg.inv(b_sensor0), b_sensor1)
+            angle_tf_1 = np.dot(np.linalg.inv(self.s0_to_s1_i), s0_to_s1)
+
+            ax1, angle_1, pt1 = td.axangles.aff2axangle(angle_tf_1)
+
+            msg = std_msgs.msg.Float32()
+            msg.data = angle_1 * (180 / np.pi)
+
+            self.joint_angle.publish(msg)
+
 
 if __name__ == '__main__':
-    try:
-        jnode()
-    except rospy.ROSInterruptException:
-        pass
+    rospy.init_node('joint_node', anonymous=True)
+    jnode = JointAngle()
+    jnode.angle_calc()
+    rospy.spin()
