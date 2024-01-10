6728d8547385684e33f8d0ffd5a7b0298ec5db48
Katelyn <katelynslee@gmail.com>
2024-01-09 22:49:51 -0500

    all joints but sensor orientation suspicious

--- nodes/trakstar_mounting_calibration.py
+++ nodes/trakstar_mounting_calibration.py
@@ -10,6 +10,7 @@ class Calibrator(object):
 
 	def __init__(self):
 		self.calibration_information = {}
+		self.normal_plane = {}
 		pass
 
 	def load_file(self, filename):
@@ -29,36 +30,36 @@ class Calibrator(object):
 			return False
 		return True
 	
-	def find_finger_plane_normal(self):
+	def find_finger_plane_normal(self, joint):
 		'''
 		Calculates the plane of rotation using PCA from the sweeping_poses. 
 		'''
-		n = len(self.sweeping_poses)
+
+		n = len(self.sweeping_poses[joint])
 		sweeping_translations = np.zeros((n, 3))
 
-		for i, transform in enumerate(self.sweeping_poses):
+		for i, transform in enumerate(self.sweeping_poses[joint]):
 			translation = transform[0:3, 3]
 			sweeping_translations[i, 0:3] = translation
 		
 		pca = PCA()
 		pca.fit(sweeping_translations)
+		print(joint)
 		print("Variance explained: %s"%(pca.explained_variance_ratio_))
 		print("Plane normal: %s"%(pca.components_[2, :]))
-		print('')
-		print(pca.components_)
-		self.finger_plane_normal = pca.components_[2, :]
+		print("--")
+		self.normal_plane[joint] = pca.components_[2, :]
 
-		return True
-
-	def project_transforms_to_plane(self):
+	def rotate_to_plane(self, joint):
 		'''
 			calibration_information : dictionary with plane_rotation, plane_translation, and neutral_transform
 		'''
+
 		# Take the translation component of the first neutral pose 
-		xaxis = self.neutral_poses[0][0:3, 3]
+		xaxis = self.neutral_poses[joint][0][0:3, 3]
 		xaxis = xaxis/np.linalg.norm(xaxis)
 		# Obtain the finger_plane_normal previously calculated from PCA
-		zaxis = self.finger_plane_normal
+		zaxis = self.normal_plane[joint]
 		# Calculate y-axis by taking the cross product 
 		yaxis = np.cross(zaxis, xaxis)
 		yaxis = yaxis/np.linalg.norm(yaxis)
@@ -68,40 +69,57 @@ class Calibrator(object):
 
 		# Define R, the rotation matrix with the new coordinate axes 
 		R = np.vstack((xaxis, yaxis, zaxis)).T
-		print("this is R ")
-		print(R)
 		plane_rotation = td.affines.compose(np.zeros(3), R, np.ones(3))
 
+		return plane_rotation
+
+	def translate_to_plane(self, joint): 
+
 		# Rotate the neutral and sweeping poses to align with the plane
-		self.neutral_poses_rotated = []
-		self.sweeping_poses_rotated = []
+		plane_rotation = self.rotate_to_plane(joint)
+		sweeping_poses_rotated = []
 		total_z = 0
 		
-		for t in self.neutral_poses:
+		for t in self.sweeping_poses[joint]:
 			rotated_transform = np.dot(np.linalg.inv(plane_rotation), t)
-			self.neutral_poses_rotated.append(rotated_transform)
-		
-		for t in self.sweeping_poses:
-			rotated_transform = np.dot(np.linalg.inv(plane_rotation), t)
-			self.sweeping_poses_rotated.append(rotated_transform)
+			sweeping_poses_rotated.append(rotated_transform)
 			
 			# After rotating the sweeping pose to align with the plane, get the z-axis value of the transform 
 			total_z += rotated_transform[2, 3]
 		
 		# Define the translation into the plane as the mean z-axis value of all the sweeping_poses
-		mean_z = total_z / len(self.sweeping_poses)
+		mean_z = total_z / len(self.sweeping_poses[joint])
 		plane_translation = td.affines.compose((0,0,-mean_z), np.eye(3), np.ones(3))
 
-		# Define total length of finger using the neutral transform
-		x = self.neutral_poses_rotated[0][0, 3]
-		y = self.neutral_poses_rotated[0][1, 3]
-		total_length = np.sqrt(x**2 + y**2)
+		return plane_translation
+	
+	def rotate_neutral_pose(self, joint):
 
+		plane_rotation = self.rotate_to_plane(joint)
+		neutral_poses_rotated = []
+		for t in self.neutral_poses[joint]:
+			rotated_transform = np.dot(np.linalg.inv(plane_rotation), t)
+			neutral_poses_rotated.append(rotated_transform)
+		
+		return neutral_poses_rotated
+
+	def project_transforms_to_plane(self, joint):
+		self.find_finger_plane_normal(joint)
+		plane_rotation = self.rotate_to_plane(joint)
+		plane_translation = self.translate_to_plane(joint)
+		neutral_poses_rotated = self.rotate_neutral_pose(joint)
 		# Save all calibration information to a dictionary `calibration_information`
-		self.calibration_information["plane_rotation"] = np.linalg.inv(plane_rotation)
-		self.calibration_information["plane_translation"] = np.linalg.inv(plane_translation)
-		self.calibration_information["neutral_transform"] = self.neutral_poses_rotated[0]
-		self.calibration_information["total_length"] = total_length
+		joint_calibration_information = {}
+		joint_calibration_information["plane_rotation"] = np.linalg.inv(plane_rotation)
+		joint_calibration_information["plane_translation"] = np.linalg.inv(plane_translation)
+		joint_calibration_information["neutral_transform"] = neutral_poses_rotated[0]
+
+		return joint_calibration_information
+	
+	def store_joint_transforms(self):
+		
+		self.calibration_information["mcp"] = self.project_transforms_to_plane("mcp")
+		self.calibration_information["dip"] = self.project_transforms_to_plane("dip")
 
 		return True
 	
@@ -118,9 +136,7 @@ def main(args):
 	calibrator = Calibrator()
 	if not calibrator.load_file(args.file):
 		exit()
-	if not calibrator.find_finger_plane_normal():
-		exit()
-	if not calibrator.project_transforms_to_plane():
+	if not calibrator.store_joint_transforms():
 		exit()
 	calibrator.save_calibration_information(args.outfile)
 
